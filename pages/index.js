a71fba12c76aa44ee3a0066700d3ddfae8c39c83
abhayChandra01 <abhayxelp@gmail.com>
2022-12-15 18:28:40 +0530

    link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
 
+// https://preview.themeforest.net/item/retro-one-page-resumeportfolio-template/full_screen_preview/26328816?_ga=2.55382059.1854934634.1671097018-24309560.1666957149
+
 export default function Home() {
   return (
     <div
